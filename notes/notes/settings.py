b0cf959dced7c1552179b1892f9cb06ca4b04481
Anubhab Patnaik <anubhabr50@gmail.com>
2022-02-23 13:36:34 +0530

    initialized integrated repository to host

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -62,6 +63,7 @@ CORS_ALLOWED_ORIGINS = [
 
 
     "http://localhost:3000",
+    "http://localhost:5000",
 
 ]
 
@@ -70,7 +72,7 @@ ROOT_URLCONF = 'notes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
